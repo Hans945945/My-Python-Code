0a9a3cb29948205ef3397fbd9593e386df7bec56
Hans945945 <hansjiang1017@gmail.com>
2025-01-29 11:59:55 +0800

    Update d768.py

--- d768.py
+++ d768.py
@@ -1,3 +1,4 @@
+#bfs
 from collections import deque
 def bfs(start):
     Q = deque([i])
@@ -32,6 +33,33 @@ while True:
             if not bfs(i):
                 BC = False
                 break
-    print("BICOLORABLE." if BC else "NOT BICOLORABLE.")
+#dfs
+'''
+def dfs(now, c):
+    color[now] = c
     
+    for v in MAP[now]:
+        if color[v] == -1:
+            if dfs(v, 1-color[now]) == False:
+                return False
+        elif color[v] == color[now]:
+            return False
+    return True
+
+while True:
+    n = int(input())
+    if n == 0:
+        break
+    MAP = [[] for _ in range(n)]
+    m = int(input())
+    for _ in range(m):
+        a,b = map(int, input().split())
+        MAP[a].append(b)
+        MAP[b].append(a)
+        
+    color = [-1]*n
+ 
+    print("BICOLORABLE." if dfs(0, 0) else "NOT BICOLORABLE.")
+    print("BICOLORABLE." if BC else "NOT BICOLORABLE.")
+'''
     
